847887da6d9175648054e29dfdc54daa67a71696
Burjwok Paulo Deng Ajang <burypaulo360@gmail.com>
2025-10-08 16:00:03 +0000

    Update authRoutes.js

--- Backend/Routes/authRoutes.js
+++ Backend/Routes/authRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { body, validationResult } from "express-validator";
-import User from "../models/User.js";
+import User from "../Models/User.js";
 import generateTokenAndSetCookie from "../utils/generateToken.js";
 import { auth, admin } from "../middleware/auth.js";
 
